f1caf4d02a90f78546eb606683fcef89336ccc02
Tobias Refsholt <tobias@refsholt.net>
2023-08-22 15:43:56 +0200

    Finished drawBoard() and added some styling

--- script.js
+++ script.js
@@ -82,12 +82,48 @@ let piecesPosition = {
 updateView()
 
 function updateView() {
-    drawBoard();
+    app.innerHTML = /*html*/ `
+        <div class="chess-board-wrapper">
+            ${drawBoard()}
+            <div class="x-axis"><span>A</span><span>B</span><span>C</span><span>D</span><span>E</span><span>F</span><span>G</span><span>H</span></div>
+            <div class="y-axis"><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span><span>7</span><span>8</span></div>
+        </div>
+    `
     placePieces();
 }
 
 function drawBoard() {
+    let boardHtml = '';
 
+    boardHtml += /*html*/`<div class="chess-board">`;
+
+    for (let rowCount = 0; rowCount < 8; rowCount++) {
+        let parity = (rowCount%2 == 0) ? 'even' : 'odd';
+        boardHtml += drawBoardRow(parity, rowCount);
+    }
+
+    boardHtml += /*html*/`</div>`;
+
+    return boardHtml;
+}
+
+function drawBoardRow(rowParity, rowCount) {
+    let html = '';
+    let CellParityOffest = 0;
+
+    if (rowParity == 'even') {
+        CellParityOffest = 1;
+    }
+
+    for (let colCount = 0; colCount < 8; colCount++) {
+        let cellColor = ( (colCount+CellParityOffest) % 2 == 0 ) ? 'black' : 'white';
+
+        html += /*html*/`
+        <div class="cell cell-${cellColor}" row="${rowCount}" col="${colCount}"></div>
+        `;
+    }
+
+    return html;
 }
 
 function placePieces() {
