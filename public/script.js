d3b791a4eb6b3e28e8e382dd0f4405e04a7bfe13
Tobias Refsholt <tobias@refsholt.net>
2023-09-18 14:02:35 +0200

    Show disabled pieces

--- public/script.js
+++ public/script.js
@@ -92,8 +92,8 @@ function activeGameHTML() {
             <div class="x-axis"><span>A</span><span>B</span><span>C</span><span>D</span><span>E</span><span>F</span><span>G</span><span>H</span></div>
             <div class="y-axis"><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span><span>7</span><span>8</span></div>
             <div class="sidebar">
-                disabledPiecesHTML("white")
-                disabledPiecesHTML("black")
+                ${disabledPiecesHTML("white")}
+                ${disabledPiecesHTML("black")}
             </div>
         </div>
     `;
