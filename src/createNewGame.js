bfcf10e8fd1f725e63ee0f29686d41cd82bc52aa
tobias <tobias@refsholt.net>
2023-12-31 17:07:30 +0100

    Added firstMove field to all pieces

--- src/createNewGame.js
+++ src/createNewGame.js
@@ -33,90 +33,90 @@ function getInitialPiecesState() {
     piecesState.push({
         position: {
             x: 3, y: 1
-        }, type: "bishop", color: "white"
+        }, type: "bishop", color: "white", firstMove: true,
     })
     piecesState.push({
         position: {
             x: 6, y: 1
-        }, type: "bishop", color: "white"
+        }, type: "bishop", color: "white", firstMove: true,
     })
     piecesState.push({
         position: {
             x: 3, y: 8
-        }, type: "bishop", color: "black"
+        }, type: "bishop", color: "black", firstMove: true,
     })
     piecesState.push({
         position: {
             x: 6, y: 8
-        }, type: "bishop", color: "black"
+        }, type: "bishop", color: "black", firstMove: true,
     })
 
     // Knights
     piecesState.push({
         position: {
             x: 2, y: 1
-        }, type: "knight", color: "white"
+        }, type: "knight", color: "white", firstMove: true,
     })
     piecesState.push({
         position: {
             x: 7, y: 1
-        }, type: "knight", color: "white"
+        }, type: "knight", color: "white", firstMove: true,
     })
     piecesState.push({
         position: {
             x: 2, y: 8
-        }, type: "knight", color: "black"
+        }, type: "knight", color: "black", firstMove: true,
     })
     piecesState.push({
         position: {
             x: 7, y: 8
-        }, type: "knight", color: "black"
+        }, type: "knight", color: "black", firstMove: true,
     })
 
     // Rooks
     piecesState.push({
         position: {
             x: 1, y: 1
-        }, type: "rook", color: "white"
+        }, type: "rook", color: "white", firstMove: true,
     })
     piecesState.push({
         position: {
             x: 8, y: 1
-        }, type: "rook", color: "white"
+        }, type: "rook", color: "white", firstMove: true,
     })
     piecesState.push({
         position: {
             x: 1, y: 8
-        }, type: "rook", color: "black"
+        }, type: "rook", color: "black", firstMove: true,
     })
     piecesState.push({
         position: {
             x: 8, y: 8
-        }, type: "rook", color: "black"
+        }, type: "rook", color: "black", firstMove: true,
     })
 
     // Queens
     piecesState.push({
         position: {
             x: 4, y: 1
-        }, type: "queen", color: "white"
+        }, type: "queen", color: "white", firstMove: true,
     })
     piecesState.push({
         position: {
             x: 4, y: 8
-        }, type: "queen", color: "black"
+        }, type: "queen", color: "black", firstMove: true,
     })
 
     // Kings
     piecesState.push({
         position: {
             x: 5, y: 1
-        }, type: "king", color: "white"
+        }, type: "king", color: "white", firstMove: true,
     })
     piecesState.push({
         position: {
             x: 5, y: 8
-        }, type: "king", color: "black"
+        }, type: "king", color: "black", firstMove: true,
     })
 
     return piecesState;
